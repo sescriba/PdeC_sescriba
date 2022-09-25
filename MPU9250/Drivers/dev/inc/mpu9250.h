dfa3e1cef82dd04f78319d215d669247e45ba149
Santiago <escribasan@gmail.com>
2022-09-25 18:28:25 -0300

    Init files

---
+++ MPU9250/Drivers/dev/inc/mpu9250.h
@@ -0,0 +1,15 @@
+/*
+ * mpu9250.h
+ *
+ *  Created on: 22 sep. 2022
+ *      Author: Santiago
+ */
+
+#ifndef DEV_INC_MPU9250_H_
+#define DEV_INC_MPU9250_H_
+
+
+
+
+
+#endif /* DEV_INC_MPU9250_H_ */
---
+++ MPU9250/src/main.c
@@ -0,0 +1,6 @@
+/*
+ * main.c
+ *
+ *  Created on: Sep 25, 2022
+ *      Author: Santiago
+ */
